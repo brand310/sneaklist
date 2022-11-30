cafd68b92b15ab8ed53e74b4530a62ff0e12c91b
brand310 <brandonsandoval0@gmail.com>
2022-11-29 19:20:35 -0800

    Added Navbar with css and new font

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Sneaklist</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Shoe</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
