45446d452c9dcd6399701f9b57a3e117e89cdf62
brand310 <brandonsandoval0@gmail.com>
2022-12-01 22:51:10 -0800

    Added changes to navbar and home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <h2 className="introName">SNEAKLIST</h2>
+      <p className="intro1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Morbi quis commodo
+        odio aenean. Pulvinar elementum integer enim neque. Odio ut enim blandit
+        volutpat. Ut sem viverra aliquet eget sit amet. Vitae et leo duis ut
+        diam quam nulla. Eu tincidunt tortor aliquam nulla facilisi. Amet
+        venenatis urna cursus eget nunc scelerisque. Lorem sed risus ultricies
+        tristique nulla aliquet enim. A pellentesque sit amet porttitor. Eu
+        tincidunt tortor aliquam nulla facilisi. Nec ultrices dui sapien eget
+        mi. Mauris in aliquam sem fringilla ut morbi tincidunt. Congue eu
+        consequat ac felis donec et odio pellentesque diam. Suspendisse sed nisi
+        lacus sed.
+      </p>
+      <div className="card1"></div>
+      <button className="button1">TRY NOW</button>
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>▪SNEAKLIST▪</h1>
+      <h1>SNEAKLIST</h1>
       <div className="links">
         <a href="/">Home </a>
         <a href="/create">New Shoe</a>
