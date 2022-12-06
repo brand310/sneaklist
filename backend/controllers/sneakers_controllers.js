117e0a88e84502e4db0be1d77af5e1813798bb75
Fabian Soriano <fabo.soriano@gmail.com>
2022-12-06 00:53:16 -0800

    added functionality to Index and Show routes

--- backend/controllers/sneakers_controllers.js
+++ backend/controllers/sneakers_controllers.js
@@ -1,27 +1,36 @@
 const express = require('express')
 const sneakers = express.Router()
 const Sneaker = require('../models/sneaker')
+const mongoose = require('mongoose')
 
 // SNEAKERS INDEX
-sneakers.get('/', (req, res) => {
-    res.json({message: 'GET stub all sneakers'})
+sneakers.get('/', async (req, res) => {
+    const sneakers = await Sneaker.find().sort({name: 1})
+    res.status(200).json(sneakers)
 })
 
 // GET to SHOW a single sneaker
-sneakers.get('/:id', (req, res) => {
-    res.json({message: 'GET stub single sneaker'})
+sneakers.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({error: 'Sneaker does not exist.'})
+    }
+    const sneaker = await Sneaker.findById(req.params.id)
+    if (!sneaker) {
+        return res.status(400).json({error: 'Sneaker does not exist.'})
+    }
+    res.status(200).json(sneaker)
 })
 
 // POST to CREATE a new sneaker
 sneakers.post('/', async (req, res) => {
-    const { name, brand, size, image, comment } = req.body
+    const { name, brand, size, image, comment } = req.body    
+    // Adding document to db
     try {
         const sneaker = await Sneaker.create(req.body)
         res.status(200).json(sneaker)
     } catch(error){
         res.status(400).json({error: error.message})
-    }
-    // res.json({message: 'POST a new sneaker'})
+    }    
 })
 
 // PUT to UPDATE a sneaker
