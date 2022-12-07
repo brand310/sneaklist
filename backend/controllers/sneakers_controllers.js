685c4bd1d268414dab985cf9f8726c61b7b05b5e
Fabian Soriano <fabo.soriano@gmail.com>
2022-12-06 19:26:21 -0800

    added remaining status numbers

--- backend/controllers/sneakers_controllers.js
+++ backend/controllers/sneakers_controllers.js
@@ -29,18 +29,18 @@ sneakers.post('/', async (req, res) => {
         const sneaker = await Sneaker.create(req.body)
         res.status(201).json(sneaker)
     } catch(error){
-        res.status(400).json({error: error.message})
+        res.status(404).json({error: error.message})
     }    
 })
 
 // PUT to UPDATE a sneaker
 sneakers.put('/:id', async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({error: 'Sneaker does not exist.'})
+        return res.status(404).json({error: 'Sneaker does not exist.'})
     }
     const sneaker = await Sneaker.findByIdAndUpdate(req.params.id, req.body)
     if (!sneaker) {
-        return res.status(400).json({error: 'Sneaker does not exist.'})
+        return res.status(404).json({error: 'Sneaker does not exist.'})
     }
     res.status(202).json(sneaker)
 })
@@ -48,11 +48,11 @@ sneakers.put('/:id', async (req, res) => {
 // DELETE a sneaker
 sneakers.delete('/:id', async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({error: 'Sneaker does not exist.'})
+        return res.status(404).json({error: 'Sneaker does not exist.'})
     }
     const sneaker = await Sneaker.findByIdAndDelete(req.params.id)
     if (!sneaker) {
-        return res.status(400).json({error: 'Sneaker does not exist.'})
+        return res.status(404).json({error: 'Sneaker does not exist.'})
     }
     res.status(204).json(sneaker)
 })
