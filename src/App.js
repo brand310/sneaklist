c1e0736d58515fd98ad192ae7fbc0f952216ae8e
Charlie <ware.charlie914@gmail.com>
2022-12-06 21:43:04 -0800

    Added create component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import React from "react";
+import Create from "./Create";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+      <Routes>
+        <Route path="/" element={ <Home/>}></Route>
+        <Route path="/sneakers/create" element={ <Create/> }/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,13 @@
+import React from "react";
+
 const Navbar = () => {
+
   return (
     <nav className="navbar">
       <h1>SNEAKLIST</h1>
       <div className="links">
-        <a href="/">Home </a>
-        <a href="/create">New Shoe</a>
+        <a href="/">Home</a>
+        <a href="/sneakers/create">New Shoe</a>
       </div>
     </nav>
   );
