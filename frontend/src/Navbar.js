7b501b8ad191eebbe771edad8a273710738b2950
brand310 <brandonsandoval0@gmail.com>
2022-12-06 22:29:13 -0800

    Homepage design added

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,6 +2,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>SNEAKLIST</h1>
+      <div className="sneaklistlogo"></div>
       <div className="links">
         <a href="/">Home </a>
         <a href="/create">New Shoe</a>
